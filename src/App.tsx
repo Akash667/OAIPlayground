fe8ddabd535ce6a21f796f16e0a423b15d8c8cce
Akash R <me@akashirl.live>
2021-02-16 14:21:11 +0530

    pre-build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { Button, Card, CardContent, Input, Paper, Slider, TextField, Typography } from '@material-ui/core';
+import { Animated } from 'react-animated-css';
 
 function App() {
+
+  const [press, setPress] = useState(false);
+
+  const [valueHook,setValueHook] = useState(20);
+
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={styles.app} >
+      <Paper elevation={4} className={`${styles.menu} ${styles.box}`}>
+            <canvas className={styles.canva} >
+
+            </canvas>
+      </Paper>
+
+      <Paper elevation={4} className={`${styles.menu} ${styles.toolbar}`}>
+        <Typography id="continuous-slider" gutterBottom>
+        Size
+        </Typography>
+        <Slider value={valueHook} onChange={(event,value)=> setValueHook((value as number))} aria-labelledby="continuous-slider"/>
+      </Paper>
+
+    </div >
   );
 }
+
+
 
 export default App;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Menu(){
+
+    return(
+
+        <div>
+                This is the Menu babe
+        </div>
+
+    )
+
+
+}
+
+export default Menu;
